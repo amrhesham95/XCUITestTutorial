86171499723df8a4a5506cc001c71b97c744a2d6
amrheshamww <amr.hesham@ww.com>
2024-05-26 16:32:47 +0200

    Creating the project

---
+++ UITestTutorial/UITestTutorial/UITestTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestTutorialApp.swift
+//  UITestTutorial
+//
+//  Created by Amr Hesham on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct UITestTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
